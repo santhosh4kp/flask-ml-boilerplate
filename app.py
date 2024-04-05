999d2286921a7e82b6a9a532f1d98da80a690b1b
Santhosh Kumar K P <101238962+santhosh4kp@users.noreply.github.com>
2024-04-06 00:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def upload_image():
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
